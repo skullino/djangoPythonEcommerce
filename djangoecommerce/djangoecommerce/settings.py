b30fe807ef8aa099a98ffe83213e77a3ee01163b
skullino <40169421+skullino@users.noreply.github.com>
2018-06-13 08:33:27 -0300

    Habilitando static

--- djangoecommerce/djangoecommerce/settings.py
+++ djangoecommerce/djangoecommerce/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
